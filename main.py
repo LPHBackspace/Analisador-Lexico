dd8bcde4f79aa88dfb431c497af909a99f1b652f
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2023-10-25 11:40:16 -0300

    v2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Array Lexemas
 lexemas = []
+result = []
 palavra_atual = ''
 
 # abrir codigo
-with open('program.txt', 'r', encoding='utf-8') as arquivo:
+with open('testPrograms/program.txt', 'r', encoding='utf-8') as arquivo:
     for linha in arquivo:
         for letra in linha:
             if letra in 'abcdefghijklmnopqrstuvwxyz':
@@ -17,11 +18,71 @@ with open('program.txt', 'r', encoding='utf-8') as arquivo:
                     if letra != '':
                         lexemas.append(letra)
 
-i = len(lexemas) - 1
-while i >= 0:
-    if lexemas[i] == '' or lexemas[i] == ' ':
-        del lexemas[i]
-    i -= 1
+# removendo espaços em branco
+item = len(lexemas) - 1
+while item >= 0:
+    if lexemas[item] == ' ':
+        del lexemas[item]
+    item -= 1
 
 
-print(lexemas)
+item = len(lexemas) - 1
+i = 0
+while i < item:
+    token = ''
+    if lexemas[i] == 'senao':
+        token = 'SENAO'
+    elif lexemas[i] == 'programa':
+        token = 'PROGRAMA'
+    elif lexemas[i] == 'se':
+        token = 'SE'
+    elif lexemas[i] == 'entao':
+        token = 'ENTAO'
+    elif lexemas[i] == 'enquanto':
+        token =  'ENQUANTO'
+    elif lexemas[i] == 'faca':
+        token =  'FACA'
+    elif lexemas[i] == 'nao':
+        token =  'NAO'
+    elif lexemas[i] == 'inicio':
+        token = 'LBLOCK'
+    elif lexemas[i] == 'fim':
+        token = 'RBLOCK'
+    elif lexemas[i] == 'verdade' or lexemas[i] == 'falso':
+        token = 'BOOLEAN'
+    elif lexemas[i] == 'ler' or lexemas[i] == 'ler_varios' or lexemas[i] == 'mostrar' or lexemas[i] == 'tocar' or lexemas[i] == 'mostrar_tocar' or lexemas[i] == 'total':
+        token = 'ENQUANTO'
+    elif lexemas[i] == ':':
+        token = 'COLON'
+    elif lexemas[i] == ',':
+        token = 'COMMA'
+    elif lexemas[i] == '==' or lexemas[i] == '<>' or lexemas[i] == '<' or lexemas[i] == '<=' or lexemas[i] == '>' or lexemas[i] == '=>':
+        token = 'OPREL'
+    elif lexemas[i] == '+' or lexemas[i] == '-' or lexemas[i] == 'ou':
+        token = 'OPSUM'
+    elif lexemas[i] == '*' or lexemas[i] == '/' or lexemas[i] == '%' or lexemas[i] == 'e':
+        token = 'OPMUL'
+    elif lexemas[i] == '^':
+        token = 'OPPOW'
+    elif lexemas[i] in '0123456789':
+        token = 'INTEGER'
+    elif lexemas[i] == '“' or lexemas[i] == '”':
+        token = 'DQUOTE'
+    elif lexemas[i] == '.':
+        token = 'DOT'
+    elif lexemas[i] == '=':
+        token = 'ASSIGN'
+    elif lexemas[i] == '(':
+        token = 'LPAR'
+    elif lexemas[i] == '':
+        token = 'RPAR'
+    elif lexemas[i] == '\n':
+        token = '\n'
+    else:
+        token = 'ID'
+    result.insert(i, "Token('" + token + "'," +  " '" + lexemas[i] + "')")
+    i += 1
+
+
+#print(lexemas)
+print(result)
